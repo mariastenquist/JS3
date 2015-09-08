3a4fa6c75e696207c1d99d5af765c656f8b14b93
Student <student@RU-001.local>
2015-09-08 17:48:18 -0600

    next comment

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-
-
-
-
 // function askNumber () {
 // 	var userInput = prompt("Type in your full phone number")
 // 	if(userInput.length != 12 ){
@@ -12,12 +8,87 @@
 
 // alert(userInput)
 
-var askNumber = prompt("type number")
+// var askNumber = ""
+ // "123-456-7890"
+// postal code
+// must follow the format xxxxx OR xxxxx-xxxx
+// state abbreviation
+// must be two characters
+// must be all caps
+// married
+// must be yes or no
+// may be any capitalization: YES, Yes, yes
 
-if (askNumber.length === 12 && askNumber[3] === "-" && askNumber[7] === "-") {
-	alert("Yay thanks")
-}
-else {
-	alert("Are you having issues today?")
+
+// var askNumber = prompt("type number")
+
+// if (askNumber.length !== 12) {
+// 	alert("please insert a full number")
+// 		if (askNumber.charAt(3) !== "-" || askNumber.charAt(7) !== "-") {
+// 	 alert("please enter dashes in number")
+// 	}
+	
+// }
+
+// }
+// else {
+// 		alert("thanks for inserting correct number")
+// }
+
+// var birthDate = prompt("enter your birthday")
+
+// if (birthDate.length !== 8){
+// 	alert("please insert style xx/xx/xx")
+// } else if (birthDate.charAt(2) !== "/" || birthDate.charAt(5) !== "/") {
+// 	alert("please add slashes")
+// } else {
+// 	alert("great formatting your bday")
+// }
+
+// var postCode = prompt("type your postal code")
+
+// if (postCode.length !== 5){
+// 	alert("please enter a 5 digit postal code")
+// } else alert("thank you!")
+
+
+
+var phoneNumber = function(input) {
+	if (input.length === 12 ) {
+		alert("phone number is correct length")
+		if (askNumber.charAt(3) !== "-" || askNumber.charAt(7) !== "-")
+			alert("enter dashes into phone number")
+	} else {
+		alert("Thank you for entering phone number correctly")
+
+	}
+
 }
 
+
+
+
+
+// do ()
+
+// while (askNumber.length === !12){
+
+
+
+// function askNumber () {
+// 	var userPhoneNumber = prompt("Insert full number please")
+
+// 	if (userPhoneNumber === "hi"){
+// 		console.log('hello')
+// 	}
+// }
+
+// askNumber();
+
+// do {
+// 	var input  = prompt("Insert number")
+
+
+// }
+
+// while  ()
